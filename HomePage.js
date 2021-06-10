e1b58e671fc9bf36a5179ac55b4772c7686c1700
MYCOMPUTER\Admin <khanhnguyen7221@gmail.com>
2021-06-10 20:28:25 +0700

    Create template StorePage successful 90%

---
+++ HomePage.js
@@ -0,0 +1,7 @@
+$(document).on('click','#ban',function() {
+    document.getElementById('#nothing').style.display = 'none';
+});
+$(document).on('click','#menu',function() {
+    document.getElementById('#nothing').style.display = 'none';
+});
+console.log("ahihi")
